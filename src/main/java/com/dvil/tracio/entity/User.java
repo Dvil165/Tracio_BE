b5adb5e234292b89080327d5f56f7895943ea859
hoanglmse170482 <hoanglmse170482@fpt.edu.vn>
2025-03-08 23:18:38 +0700

    test

--- src/main/java/com/dvil/tracio/entity/User.java
+++ src/main/java/com/dvil/tracio/entity/User.java
@@ -19,6 +19,7 @@ import java.util.List;
 @Entity
 @Table(name = "users")
 public class User implements UserDetails {
+    // abcccc
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)

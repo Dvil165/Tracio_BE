d2dfba57402db4a2262eac4c245fcfb2bb1212af
hoanglmse170482 <hoanglmse170482@fpt.edu.vn>
2025-03-28 00:04:35 +0700

    get my route

--- src/main/java/com/dvil/tracio/repository/RouteRepo.java
+++ src/main/java/com/dvil/tracio/repository/RouteRepo.java
@@ -4,6 +4,9 @@ import com.dvil.tracio.entity.Route;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface RouteRepo extends JpaRepository<Route, Integer> {
+    List<Route> findByUserId(Integer userId);
 }
